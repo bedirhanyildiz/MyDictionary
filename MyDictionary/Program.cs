c9088a5acab1cda29f6c2b9d7c50c2a8c994dec7
bedirhan yıldız <bedirhanyildiz94@gmail.com>
2021-01-23 17:00:26 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Kuzey Yiğit");
+            isimler.Add("Gizem");
+
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+            List<int> sayilar = new List<int>();
+            sayilar.Add(2);
+            sayilar.Add(4);
+            Console.WriteLine(sayilar.Count);
+        }
+    }
+}
